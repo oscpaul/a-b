bd282234e6a8d3955dcf203fcaf85809c4f29121
Paul's Github <47764854+oscpaul@users.noreply.github.com>
2026-04-28 21:51:45 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,1 @@
-// next.config.js
-module.exports = {
-  async redirects() {
-    return [
-      {
-        source: '/maintenance',
-        destination: '/tune-up',
-        permanent: true, // this makes it a 301 redirect
-      },
-    ]
-  },
-}
+
